b40658bca7310c161a74095059ab818f806133c4
xiechangqing <576535019@qq.com>
2018-06-12 22:54:24 +0800

    简单的一个routerdemo

--- src/App.js
+++ src/App.js
@@ -1,20 +1,49 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <div>
+          <ul>
+            <li><Link to='/'>home</Link></li>
+            <li><Link to='/about'>about</Link></li>
+            <li><Link to='/topic'>topic</Link></li>
+          </ul>
+          <hr/>
+
+          <Route exact path='/' render={() => <h2>Home</h2>}/>
+          <Route path='/about' render={() => <h2>About</h2>}/>
+          <Route path='/topic' component={this.Topics} />
+        </div>
+      </Router>
     );
+  }
+  Topics = ({ match }) => {
+    console.log(match);
+    return (
+        <div>
+          <h2>Topics</h2>
+          <ul>
+            <li><Link to={`${match.url}/rendring`}>rendring</Link></li>
+            <li><Link to={`${match.url}/component`}>component</Link></li>
+            <li><Link to={`${match.url}/props`}>props</Link></li>
+          </ul>
+          <Route path={`${match.url}/:topicId`} component={this.Topic}/>
+          <Route exact path={`${match.url}`} render={()=> <div>please Select a topic</div>}/>
+        </div>
+    )
+  }
+  Topic = ({ match }) => {
+    console.log(match);
+    return(
+        <div>{match.params.topicId}</div>
+    )
   }
 }
 
